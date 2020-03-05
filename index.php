7fb0be123baf012f870d0abe1a3d760df176b833
Peach <zhaz_zy@yahoo.com>
2020-03-05 20:25:50 +0800

    First Push

---
+++ index.php
@@ -0,0 +1,118 @@
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>LogIn</title>
+ <link rel="stylesheet" href="css/gen.css">
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>	
+<body>
+
+
+<!--Header-->
+<nav class="navbar navbar-expand-md navbar-dark bg-dark stiky-top">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="https://www.facebook.com/leemar.garcia"><img src="image/jpg/1.jpg"  class="img-fluid rounded-circle" style="width:4rem";></a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarResponsive">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="#">Home</a>
+				</li>
+			</ul>
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="#">Apply</a>
+				</li>
+			</ul>
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="#">Galery</a>
+				</li>
+			</ul>
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="#">About</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
+
+<!--Slider-->
+<div id="slides" class="carousel slide" data-ride="carousel">
+	<ul class="carousel-indicators">
+		<li data-target="'slides" data-slide-to="0" class="active"></li>
+		<li data-target="'slides" data-slide-to="1"></li>
+		<li data-target="'slides" data-slide-to="2"></li>
+		<li data-target="'slides" data-slide-to="3"></li>
+		<li data-target="'slides" data-slide-to="4"></li>
+		<li data-target="'slides" data-slide-to="5"></li>
+	</ul>
+	<div class="carousel-inner">
+		<div class="carousel-item active">
+			<img src="https://scontent.fmnl4-1.fna.fbcdn.net/v/t1.0-9/78719424_3755454194472718_527584741734481920_o.jpg?_nc_cat=108&_nc_sid=07e735&_nc_ohc=abjX-tHl1oEAX_TFwqW&_nc_ht=scontent.fmnl4-1.fna&oh=dab44b0bc6a0e6cf872f5825f2951a35&oe=5E9839A8">
+		</div>
+		<div class="carousel-item">
+			<img src="https://i1.wp.com/blackpinkupdate.com/wp-content/uploads/2019/07/BLACKPINK-Lisa-Ragnarok-M-Eternal-Love-Game.jpg?ssl=1">
+		</div>
+		<div class="carousel-item">
+			<img src="https://gamersantai.com/wp-content/uploads/2019/07/ragnarok-m-lisa-fp.jpg">
+		</div>
+		<div class="carousel-item">
+			<img src="https://pbs.twimg.com/media/EDEiT7KUcAAq93p.jpg">
+		</div>
+		<div class="carousel-item">
+			<img src="https://bestinslot.co/wp-content/uploads/2018/09/RagnarokMobileEdited.png">
+		</div>
+	</div>
+</div>
+		
+
+<!--Footer-->
+<footer>
+<div class="container-fluid padding">
+	<div class="row text-center">
+		<div class="col-xs-12 col-sm-6 col-md-4">
+			<hr class="light">
+			<p><h3>Who we need</h3></p>
+			<hr class="light">
+			<p>- Discord User</p>
+			<p>- Minimum 1k+ gold medal</p>
+			<p>- Minimum 700k contribution</p>
+			<p>- Discord User</p>
+			<p>- Desire to improve character</p>
+		</div>
+		<div class="col-xs-12 col-sm-6 col-md-4">
+			<hr class="light">
+			<p><h3>What you get</h3></p>
+			<hr class="light">
+			<p>- Weekly Oracle run</p>
+			<p>- Weekly Thanatos tower run</p>
+			<p>- Weekly Valhalla run</p>
+			<p>- No drama</p>
+		</div>
+		<div class="col-xs-12 col-sm-12 col-md-4">
+			<hr class="light">
+			<p><h3>What we have</h3></p>
+			<hr class="light">
+				<p>- Vending - Level12</p>
+				<p>- Cat Cafe - Level 15</p>
+				<p>- Cat Litter - Level 15</p>
+				<p>- Sewing - Level 10</p>
+				<p>- Anvil - Level 7</p>
+				<p>- Workshop - Level 4</p>
+				<p>- Couch - Level 2</p>
+				<p>- Obelisk- Level 2</p>
+		</div>
+	</div>
+</div>
+</footer>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
